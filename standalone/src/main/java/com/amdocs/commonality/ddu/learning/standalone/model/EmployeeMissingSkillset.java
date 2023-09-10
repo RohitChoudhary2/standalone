f3fbd14bc94f86021809c83a93fe29b8cfc8740b
RohitChoudhary2 <84731099+RohitChoudhary2@users.noreply.github.com>
2023-09-10 18:59:09 +0530

    standalone validateSkills springboot project

---
+++ standalone/src/main/java/com/amdocs/commonality/ddu/learning/standalone/model/EmployeeMissingSkillset.java
@@ -0,0 +1,30 @@
+package com.amdocs.commonality.ddu.learning.standalone.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeMissingSkillset {
+	private String empId;
+	private List<String> missingSkills=new ArrayList<>();
+	
+	public String getEmpId() {
+		return empId;
+	}
+	
+	public void setEmpId(String empId) {
+		this.empId=empId;
+	}
+	
+	public List<String> getMissingSkills() {
+		return missingSkills;
+	}
+	
+	public void setMissingSkills(List<String> missingSkills) {
+		this.missingSkills=missingSkills;
+	}
+
+}
+
